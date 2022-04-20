97dd382605943edc9b1a138a929e2c3c3b64a088
hidensuport <60985137+hidensuport@users.noreply.github.com>
2022-04-20 00:33:02 -0500

    Update create.sql

--- create.sql
+++ create.sql
@@ -4,7 +4,9 @@ DROP TABLE if exists Item;
 DROP TABLE if exists Bid;
 DROP TABLE if exists Bidders;
 
-
+-- This is a category table including all categories presented in the 
+-- given data. A category_id is associated with each category and is 
+-- used to link each item with its categories in ItemCategory table.
 CREATE TABLE Category
 (
  item_id      INT          NOT NULL UNIQUE,
@@ -41,12 +43,12 @@ CREATE TABLE Item
 
 CREATE TABLE Bid
 (
- item_id    INT      NOT NULL ,
+ id INT      NOT NULL UNIQUE ,
  bidder_id VARCHAR(255)      NOT NULL,
  bid_time  datetime NOT NULL,
  amount    DOUBLE   NOT NULL,
- PRIMARY KEY (bid_time),
- FOREIGN KEY (item_id) REFERENCES USER (user_id)
+ PRIMARY KEY (id)
+
 );
  
 CREATE TABLE Bidders
